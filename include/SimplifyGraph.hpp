b15bfad445dd314bfcfb76d09892ca8bd87c240a
Aaron Fleisher <aaronfleisher@ufl.edu>
2020-08-01 10:20:11 -0400

    generated simple_graph nodelist instead of moving it.  Need to fix this.

--- include/SimplifyGraph.hpp
+++ include/SimplifyGraph.hpp
@@ -8,15 +8,14 @@ Graph SimplifyGraph(const Graph& graph, Directionality directionality) {
     /* BUILD GRAPH */
     Graph simple_graph;
     simple_graph.directionality = directionality;
+    ID i (0);
     /* CREATE EDGES */
     switch(directionality) {
     case undirected: {
         std::set<std::set<Node>> edge_set;
         std::for_each(std::execution::unseq, cRANGE(graph.edges), [&edge_set](const auto& edge) {
-            if(edge.from != edge.to) { 
-                std::set<Node> nodes ({edge.from, edge.to});
-                edge_set.emplace(nodes); } });
-        ID i (0);
+            std::set<Node> nodes ({edge.from, edge.to});
+            edge_set.emplace(nodes); });
         std::for_each(cRANGE(edge_set), [&i, &simple_graph](const auto& edge){
             Nodelist edge_vec (cRANGE(edge));
             Edge edge_write ({.id = i, .from = edge_vec[0], .to = edge_vec[1] });
@@ -28,7 +27,6 @@ Graph SimplifyGraph(const Graph& graph, Directionality directionality) {
             if(edge.from != edge.to) { 
                 Nodelist nodes ({edge.from, edge.to});
                 edge_set.emplace(nodes); } });
-        ID i (0);
         std::for_each(cRANGE(edge_set), [&i, &simple_graph](const auto& edge){
             Edge edge_write ({.id = i, .from = edge[0], .to = edge[1] });
             simple_graph.edges.emplace_back(edge_write); 
@@ -40,7 +38,8 @@ Graph SimplifyGraph(const Graph& graph, Directionality directionality) {
         case 100'001 ... 30'000'000 : { simple_graph.graph_size = big;     break; };
         default : {                     simple_graph.graph_size = giant;   break; }; }
     /* COPY NODES   */
-    simple_graph.nodes = std::move(graph.nodes);
+    // simple_graph.nodes = std::move(graph.nodes);
+    simple_graph.nodes = GenNodelist(graph.edges);
     /* RETURN GRAPH */
     return simple_graph; }
 #endif//PANGOLIN_SIMPLIFYGRAPH_HPP
